401d043cf05b67aa12d0c780cece073f4b0eb667
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 22:20:21 -0700

    created a method to check if given array contains the triplet whose sum is equals to x

--- src/TripletSumInArray.java
+++ src/TripletSumInArray.java
@@ -1,3 +1,27 @@
+import java.util.Arrays;
+
 public class TripletSumInArray {
-    
+
+    public static boolean tripletSumInArray(int [] arr, int n, int x){
+        int i, j, k;
+        Arrays.sort(arr);
+        for(i=0; i<n; i++){
+            j= i+1;
+            k= n-1;
+            while(j<k){
+                int sum = arr[i]+ arr[j] + arr[k];
+                if(sum==k){
+                    return true;
+                }
+                else if(sum >k){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }return false;
+    }
+    public static void main(String[] args) {
+
+    }
 }
