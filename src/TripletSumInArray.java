81299cd84f34a51f48a6d75940660d2872edfadc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 22:22:53 -0700

    added comments for the better understanding,

--- src/TripletSumInArray.java
+++ src/TripletSumInArray.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 
 public class TripletSumInArray {
-
+// method to check if given array contains the triplet whose sum is equals to x
     public static boolean tripletSumInArray(int [] arr, int n, int x){
         int i, j, k;
+        //firstly sort the array
         Arrays.sort(arr);
+        //run a loop from i to n
         for(i=0; i<n; i++){
+            // i and then j and k start from the end
             j= i+1;
             k= n-1;
+            //move j and k accordingly
             while(j<k){
                 int sum = arr[i]+ arr[j] + arr[k];
                 if(sum==k){
@@ -18,7 +22,7 @@ public class TripletSumInArray {
                 }else{
                     j++;
                 }
-            }
+            }//if nothing matches return false
         }return false;
     }
     public static void main(String[] args) {
